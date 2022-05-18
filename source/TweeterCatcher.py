4d9acfa5a7af4626ced8d831e6c7c7c264800b87
NatanAmaro <47829269+NatanAmaro@users.noreply.github.com>
2022-05-17 21:33:22 -0300

    first commit

---
+++ source/TweeterCatcher.py
@@ -0,0 +1,20 @@
+import tweepy
+from kafka import KafkaProducer
+from source.constants import *
+from json import dumps
+
+
+def run():
+    auth = tweepy.OAuth1UserHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    producer = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS, value_serializer=lambda K: dumps(K).encode('utf-8'))
+    api = tweepy.API(auth)
+    tweets = tweepy.Cursor(api.search_tweets, q="#santander", tweet_mode='extended')
+
+    for tweet in tweets:
+        print(tweet)
+
+
+if __name__ == '__main__':
+    run()
